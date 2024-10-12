815e5b1b87380d402eefa4d10b208084ba48374a
sidnei <sidneiferreirati@gmail.com>
2024-10-12 18:07:41 -0300

    Atualizando o status do ticket

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,3 +1,6 @@
 export function updateStatus({ request, response, database }) {
-  return response.end("Ok");
+  const { id } = request.params;
+
+  database.update("tickets", id, { status: "closed", updated_at: new Date() });
+  return response.end();
 }
