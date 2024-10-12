ab7aa9475884f998d96beddabb1d85f074e55c54
sidnei <sidneiferreirati@gmail.com>
2024-10-12 16:39:48 -0300

    filtrando pelo status

--- src/database/database.js
+++ src/database/database.js
@@ -29,7 +29,16 @@ export class Database {
     this.#persist();
   }
 
-  select(table) {
-    return this.#database[table];
+  select(table, filters) {
+    let data = this.#database[table] ?? [];
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+    return data;
   }
 }
